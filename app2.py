89cb9e300c8c2104b135a07bb6c8b547dc3ac78b
indrka <pepihin@iteranet.ru>
2024-07-23 20:54:07 +0300

    add new youtube-download lib

--- app2.py
+++ app2.py
@@ -1,14 +1,13 @@
 import os
-import glob
 import asyncio
 import discord
-import yt_dlp
 from discord.ext import commands
 from dotenv import load_dotenv
 from config import *
 import random
 from queue_manager import local_queue
 from youtube_dl import YoutubeDL
+import yt_dlp
 
 load_dotenv()
 lq = local_queue()
@@ -34,6 +33,20 @@ async def e(ctx, password):
         exit(1)
         
 
+@bot.command()
+async def cc(ctx):
+    await ctx.send('p!play Never Gonna Give You Up')
+
+
+@bot.command(aliases=['t'])
+async def tinkoff(ctx, phrase_number=0):
+    if not phrase_number:
+        phrase_number = random.randint(1, 115)
+        await local_play(ctx, f'audios/{phrase_number}.mp3')
+    else:
+        await local_play(ctx, f'audios/{phrase_number}.mp3')
+
+
 @bot.command(aliases=['q'])
 async def queue(ctx):
     queue = lq.get_songs_queue()
@@ -52,55 +65,70 @@ async def queue(ctx):
 
 
 @bot.command(aliases=['p', 'з'])
-async def play(ctx, url=DEFAULT_SONG, loop=False):
+async def rick(ctx):
     voice_channel = ctx.author.voice.channel
     if ctx.voice_client is None:
         await voice_channel.connect()
 
     if ctx.voice_client.is_playing():
-        await ctx.send(f'{ctx.message.author.mention}, i will add your song in common queue')
-        lq.add_song("song_name", url)
+        await ctx.send("something is currently playing...")
+        return
 
-    else:
+    FFMPEG_OPTIONS = {
+        'before_options':
+        '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 200M',
+        'options': '-vn'
+    }
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'quiet': True,
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        secret = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        info = ydl.extract_info(secret, download=False)
+        url2 = info['url']
+        print(url2)
+        source = discord.FFmpegPCMAudio(url2)
+        vc = ctx.voice_client
+        vc.play(source)
 
-        FFMPEG_OPTIONS = {'before_options':'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 200M','options': '-vn'}
-        YDL_OPTIONS = {'format': 'bestaudio/best', 'outtmpl': 'audios/%(extractor)s-%(id)s-%(title)s.%(ext)s','quiet': True, 'postprocessors': [{ 'key': 'FFmpegExtractAudio','preferredcodec': 'mp3','preferredquality': '192',}],}
-        
-        with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=True)
-            song_url = info['url']
-            print(song_url)
-            source = discord.FFmpegPCMAudio(source=song_url, options=FFMPEG_OPTIONS)
-            vc = ctx.voice_client
-            lq.add_song("song_name", url)
-            vc.play(source)
+
+# @bot.command(aliases=['здфнд', 'pl', 'зд'])
+# async def playl(ctx, url=DEFAULT_SONG):
+#     await play(ctx, url, True)
             
-            while vc.is_playing():
-                await asyncio.sleep(1)
-            if not vc.is_paused():
-                if not loop:
-                    lq.delete_song()   
-                while len(lq.get_songs_queue()) != 0:
-                    song_url = lq.get_songs_queue()[0]['song_name']
-                    if not loop:
-                        lq.delete_song()             
-                    await play(ctx, song_url)
-                await asyncio.sleep(300)
-                await clean(ctx)
-                await vc.disconnect()
 
+# @bot.command()
+# async def local_play(ctx, path_to_song):
+#     await join(ctx)
 
-@bot.command(aliases=['здфнд', 'pl', 'зд'])
-async def playl(ctx, url=DEFAULT_SONG):
-    await play(ctx, url, True)
-            
+#     guild = ctx.message.guild
+#     voice_client = guild.voice_client
+#     voice_client.play(
+#         discord.FFmpegPCMAudio(path_to_song)
+#     )
+#     voice_client.source = discord.PCMVolumeTransformer(voice_client.source, 1)
+
+#     while voice_client.is_playing():
+#         await asyncio.sleep(1)
+#     else:
+#         await asyncio.sleep(15)
+#         while voice_client.is_playing():
+#             break
+#         else:
+#             await voice_client.disconnect()
+
 
 @bot.command(aliases=['c', 'с'])
-async def clean(ctx):
-    pattern = "audios/*"
-    files = glob.glob(pattern)
-    for file in files:
-        os.remove(file)
+async def cycle(ctx):
+
+    await ctx.voice_client.disconnect()
 
 
 @bot.command(aliases=['n'])
